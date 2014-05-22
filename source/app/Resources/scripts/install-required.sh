2a8f3496432b711abf050d32c73b189f7c5f4a37
rpour <info@rpour.de>
2014-05-22 21:29:04 +0200

    Added php5-xdebug and codesniffer installation.

--- source/app/Resources/scripts/install-required.sh
+++ source/app/Resources/scripts/install-required.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sudo apt-get install curl sqlite php5 php5-cli php5-sqlite php5-curl php5-intl
+sudo apt-get install curl sqlite \
+    php5-xdebug php5 php5-cli php5-sqlite php5-curl php5-intl \
+    php-pear
+
+sudo pear install PHP_CodeSniffer
